573b4d45a9f4d2c82dbf5031d412d971eeace267
Jay Van Dam <hello@jayvandam.net>
2022-07-15 14:50:05 -0400

    commit

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
+
+// Import components
 import { StyleSheet, Text, View } from 'react-native';
 
+
+/* 
+App Component
+
+Root component for a UI that React Native uses
+*/
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
+      <Text>Hi</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/* 
+Stylesheet Object
+*/
 const styles = StyleSheet.create({
   container: {
+    // Similar to CSS
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
