a68e6c1100907315b41cc72b11f750a6213d8635
Jay Van Dam <hello@jayvandam.net>
2022-07-18 16:38:11 -0400

    initial add flexbox

--- App.js
+++ App.js
@@ -20,8 +20,11 @@ export default function App() {
     <View style={styles.appContainer}>
 
       {/* Goal Input */}
-      <View>
-        <TextInput placeholder='Enter goal...' />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Enter goal...'
+        />
         <Button title='Add Goal' />
       </View>
 
@@ -39,6 +42,15 @@ Stylesheet Object
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%'
   }
 
 });
