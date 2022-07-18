0f607a5d2fe90db0d0f3115fd2f198f64aafdcc4
Jay Van Dam <hello@jayvandam.net>
2022-07-18 15:25:27 -0400

    layout/flexbox testing start

--- App.js
+++ App.js
@@ -16,19 +16,8 @@ Root component for a UI that React Native uses
 */
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text
-          style={styles.dummyText}
-        >
-          More text.</Text>
-      </View>
-      <Text
-        style={styles.dummyText}
-      >
-        Hello World!</Text>
-      <Button title='TapME'/>
-      <StatusBar style="auto" />
+    <View>
+
     </View>
   );
 }
@@ -37,17 +26,5 @@ export default function App() {
 Stylesheet Object
 */
 const styles = StyleSheet.create({
-  container: {
-    // Similar to CSS
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummyText: {
-    margin: 16,
-    borderWidth: 1,
-    borderColor: 'blue',
-    padding: 16
-  }
+
 });
