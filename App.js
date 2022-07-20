e4b0f07863255e23385244bbe14bb682433db974
Jay Van Dam <hello@jayvandam.net>
2022-07-20 10:18:14 -0400

    continue building goals app

--- App.js
+++ App.js
@@ -3,120 +3,122 @@ import { StatusBar } from "expo-status-bar";
 // Import components
 import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
-/* 
-Flexbox Practice
+/*
+App Component
+
+Root component for a UI that React Native uses
 */
 export default function App() {
   return (
-    <View style={{
-      padding: 50,
-      flexDirection: 'row',
-      width: '80%',
-      height: 300,
-      justifyContent: 'space-around',
-      alignItems: 'stretch'
-    }}>
-      <View
-        style={{
-          backgroundColor: "red",
-          flex: 1,
-          // width: 100,
-          // height: 100,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>1</Text>
+    <View style={styles.appContainer}>
+
+      {/* Goal Input */}
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Enter goal...'
+        />
+        <Button title='Add Goal (+)' />
       </View>
-      <View
-        style={{
-          backgroundColor: "blue",
-          flex: 1,
-          // width: 100,
-          // height: 100,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: "green",
-          flex: 2,
-          // width: 100,
-          // height: 100,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>3</Text>
+
+      {/* Goal List */}
+      <View>
+        <Text>Goal List</Text>
       </View>
     </View>
   );
 }
 
-/* 
+/*
 Stylesheet Object
 */
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    padding: 50
   },
   inputContainer: {
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "space-between"
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "80%",
-    marginRight: 8,
-  },
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8
+  }
+
 });
 
-// /*
-// App Component
-
-// Root component for a UI that React Native uses
+// /* 
+// Flexbox Practice
 // */
 // export default function App() {
 //   return (
-//     <View style={styles.appContainer}>
-
-//       {/* Goal Input */}
-//       <View style={styles.inputContainer}>
-//         <TextInput
-//           style={styles.textInput}
-//           placeholder='Enter goal...'
-//         />
-//         <Button title='Add Goal (+)' />
+//     <View style={{
+//       padding: 50,
+//       flexDirection: 'row',
+//       width: '80%',
+//       height: 300,
+//       justifyContent: 'space-around',
+//       alignItems: 'stretch'
+//     }}>
+//       <View
+//         style={{
+//           backgroundColor: "red",
+//           flex: 1,
+//           // width: 100,
+//           // height: 100,
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <Text>1</Text>
 //       </View>
-
-//       {/* Goal List */}
-//       <View>
-//         <Text>Goal List</Text>
+//       <View
+//         style={{
+//           backgroundColor: "blue",
+//           flex: 1,
+//           // width: 100,
+//           // height: 100,
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <Text>2</Text>
+//       </View>
+//       <View
+//         style={{
+//           backgroundColor: "green",
+//           flex: 2,
+//           // width: 100,
+//           // height: 100,
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <Text>3</Text>
 //       </View>
 //     </View>
 //   );
 // }
 
-// /*
+// /* 
 // Stylesheet Object
 // */
 // const styles = StyleSheet.create({
 //   appContainer: {
-//     padding: 50
+//     padding: 50,
 //   },
 //   inputContainer: {
 //     flexDirection: "row",
-//     justifyContent: "space-between"
+//     justifyContent: "space-between",
 //   },
 //   textInput: {
 //     borderWidth: 1,
-//     borderColor: '#cccccc',
-//     width: '80%',
-//     marginRight: 8
-//   }
-
+//     borderColor: "#cccccc",
+//     width: "80%",
+//     marginRight: 8,
+//   },
 // });
+
+
