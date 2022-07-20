3cedbaa85409c394815c42eada0d7dc41221f67e
Jay Van Dam <hello@jayvandam.net>
2022-07-20 13:06:10 -0400

    add goal press styling

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,11 +3,17 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 function GoalItem(props) {
   return (
     // JavaScript bind() preconfigures a function for future use
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        //   for iOS press stylings
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -16,11 +22,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
   goalText: {
     color: "white",
+    padding: 8,
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
 });
