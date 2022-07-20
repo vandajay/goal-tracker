c34f03af12f8dadd988ec9a4d9adf26d1e80100c
Jay Van Dam <hello@jayvandam.net>
2022-07-20 09:54:09 -0400

    flexbox practice

--- App.js
+++ App.js
@@ -9,30 +9,25 @@ import {
   Button
 } from 'react-native';
 
-
 /* 
-App Component
-
-Root component for a UI that React Native uses
+Flexbox Practice
 */
 export default function App() {
   return (
-    <View style={styles.appContainer}>
-
-      {/* Goal Input */}
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='Enter goal...'
-        />
-        <Button title='Add Goal' />
+    <View>
+      <View style={styles.appContainer}>
+        <Text>1</Text>
       </View>
-
-      {/* Goal List */}
-      <View>
-        <Text>Goal List</Text>
+      <View style={styles.appContainer}>
+        <Text>2</Text>
       </View>
+      <View style={styles.appContainer}>
+        <Text>3</Text>
+      </View>
+  
     </View>
+    
+    
   );
 }
 
@@ -50,7 +45,54 @@ const styles = StyleSheet.create({
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%'
+    width: '80%',
+    marginRight: 8
   }
 
 });
+
+// /* 
+// App Component
+
+// Root component for a UI that React Native uses
+// */
+// export default function App() {
+//   return (
+//     <View style={styles.appContainer}>
+
+//       {/* Goal Input */}
+//       <View style={styles.inputContainer}>
+//         <TextInput
+//           style={styles.textInput}
+//           placeholder='Enter goal...'
+//         />
+//         <Button title='Add Goal (+)' />
+//       </View>
+
+//       {/* Goal List */}
+//       <View>
+//         <Text>Goal List</Text>
+//       </View>
+//     </View>
+//   );
+// }
+
+// /* 
+// Stylesheet Object
+// */
+// const styles = StyleSheet.create({
+//   appContainer: {
+//     padding: 50
+//   },
+//   inputContainer: {
+//     flexDirection: "row",
+//     justifyContent: "space-between"
+//   },
+//   textInput: {
+//     borderWidth: 1,
+//     borderColor: '#cccccc',
+//     width: '80%',
+//     marginRight: 8
+//   }
+
+// });
